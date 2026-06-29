bfb5f52c30d3df84d04a1c88bcbc293cc2162f5e
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-29 20:54:32 +0530

    Updated The Module Doctor

--- Hospital Management Database/Backend/doctor.py
+++ Hospital Management Database/Backend/doctor.py
@@ -1,35 +1,17 @@
-import csv
-import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-DOCTOR_FILE = os.path.join(BASE_DIR, 'data/doctor.csv')
-
-HEADER = [
-    'doctor_id',
-    'name',
-    'specialisation',
-    'phone'
-]
-
-def create_file():
-
-    os.makedirs(DATA_DIR, exist_ok= True)
-
-    if not os.path.exists(DOCTOR_FILE):
-        with open(DOCTOR_FILE, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(HEADER)
+from connection import conn, cursor
            
 def doctor_exists(doctor_id):
 
-    with open(DOCTOR_FILE, 'r') as file:
-        reader = csv.DictReader(file)
+    query = 'SELECT * FROM doctors WHERE doctor_id = %s'
 
-        for row in reader:
-            if row['doctor_id'] == doctor_id:
-                return True            
- 
+    cursor.execute(query, (doctor_id,))
+
+    doctor = cursor.fetchone()
+
+    if doctor:
+        return True
+    
     return False
 
 def add_doctor():

29ccd93e25b512d6824e826088791710ea4d86c9
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-07-01 14:13:38 +0530

    Added Sample Data Sql File.

--- Hospital Management Database/Database/sample_data.sql
+++ Hospital Management Database/Database/sample_data.sql
@@ -3,12 +3,19 @@ USE hospital_db;
 -- Doctors --
 
 INSERT INTO doctors
-(doctor_name, specialization, phone)
+(doctor_id, doctor_name, specialization, phone)
 
 VALUES
-('Dr. John', 'Cardiology', '9876543210'),
-('Dr. Alice', 'Neurology', '9876543211'),
-('Dr. David', 'Orthopedics', '9876543212');
+('D001','Dr. Anil Kumar','Cardiology','9876543210'),
+('D002','Dr. Meera Nair','Neurology','9876543211'),
+('D003','Dr. Rahul Menon','Orthopedics','9876543212'),
+('D004','Dr. Priya Joseph','Pediatrics','9876543213'),
+('D005','Dr. Arjun Das','Dermatology','9876543214'),
+('D006','Dr. Neha Sharma','Gynecology','9876543215'),
+('D007','Dr. Vivek Nair','Ophthalmology','9876543216'),
+('D008','Dr. Sanjay Kumar','ENT','9876543217'),
+('D009','Dr. Asha Menon','Psychiatry','9876543218'),
+('D010','Dr. Deepak Raj','General Medicine','9876543219');
 
 -- Patients --
 
@@ -16,9 +23,16 @@ INSERT INTO patients
 (patient_name, age, gender, phone)
 
 VALUES
-('Rahul', 25, 'Male', '9991112222'),
-('Anu', 30, 'Female', '9991113333'),
-('Arjun', 42, 'Male', '9991114444');
+('P001', 'Rishan', 21,'Male', '9000000001'),
+('P002', 'Shadil', 22, 'Female', '9000000002'),
+('P003', 'Akhil', 23, 'Male', '9000000003'),
+('P004', 'Anu', 24, 'Female', '9000000004'),
+('P005', 'Meera', 25, 'Male', '9000000005'),
+('P006', 'Rahul', 26, 'Female', '9000000006'),
+('P007', 'Sneha', 27, 'Male', '9000000007'),
+('P008', 'Arjun', 28, 'Female','9000000008'),
+('P009', 'Nisha', 29, 'Male','9000000009'),
+('P010', 'Vivek', 30, 'Female','9000000010');
 
 -- Appointments --
 
